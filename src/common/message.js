1c1c5d5b912b31be1a993b9490f3582424942651
yangze8818 <yangze_lmkj@163.com>
2018-04-16 16:18:57 +0800

    Merge branch 'master' of https://github.com/yangze8818/kalix-vue-common
    # Conflicts:
    #	README.md
    message 方法修改为ES5语法

--- src/common/message.js
+++ src/common/message.js
@@ -5,24 +5,24 @@
 import {Message} from 'element-ui'
 
 export default {
-  message: function (_type, _msg, _showClose = true) {
+  message: function (_type, _msg, _showClose) {
     Message({
       type: _type,
-      showClose: _showClose,
+      showClose: _showClose | true,
       message: _msg
     })
   },
-  success: function (_msg, _showClose = true) {
-    this.message('success', _msg, _showClose)
+  success: function (_msg, _showClose) {
+    this.message('success', _msg, _showClose | true)
   },
-  warning: function (_msg, _showClose = true) {
-    this.message('warning', _msg, _showClose)
+  warning: function (_msg, _showClose) {
+    this.message('warning', _msg, _showClose | true)
   },
-  error: function (_msg, _showClose = true) {
-    this.message('error', _msg, _showClose)
+  error: function (_msg, _showClose) {
+    this.message('error', _msg, _showClose | true)
   },
-  info: function (_msg, _showClose = true) {
-    this.message('info', _msg, _showClose)
+  info: function (_msg, _showClose) {
+    this.message('info', _msg, _showClose | true)
   },
   processResult: function (res) { // 处理返回结果
     if (res.data.success) {
